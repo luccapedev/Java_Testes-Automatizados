20ca70474f1c9bb1674a3b277263b0e34eb8f3d6
Lucca Pedroza <luccaesu2003@hotmail.com>
2026-08-12 14:12:06 -0300

    mais cenários de teste

--- src/test/java/br/com/alura/adopet/api/service/CalculadoraProbabilidadeAdocaoTest.java
+++ src/test/java/br/com/alura/adopet/api/service/CalculadoraProbabilidadeAdocaoTest.java
@@ -34,4 +34,28 @@ class CalculadoraProbabilidadeAdocaoTest {
 
         Assertions.assertEquals(ProbabilidadeAdocao.ALTA,probabilidade);
     }
+
+    @Test
+    void cenario02() {
+        //idade 15 anos e 4kg - MEDIA
+
+        Abrigo abrigo = new Abrigo(new CadastroAbrigoDto(
+                "Abrigo feliz",
+                "94999999999",
+                "abrigofeliz@email.com.br"
+        ));
+        Pet pet = new Pet(new CadastroPetDto(
+                TipoPet.GATO,
+                "Miau",
+                "Siames",
+                15,
+                "Cinza",
+                4.0f
+        ), abrigo);
+
+        CalculadoraProbabilidadeAdocao calculadora = new CalculadoraProbabilidadeAdocao();
+        ProbabilidadeAdocao probabilidade = calculadora.calcular(pet);
+
+        Assertions.assertEquals(ProbabilidadeAdocao.MEDIA,probabilidade);
+    }
 }
