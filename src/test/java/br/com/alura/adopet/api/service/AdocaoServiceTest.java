1005f9d6e5adfdbc01e5f318fb4c12d1a58614c9
Lucca Pedroza <luccaesu2003@hotmail.com>
2026-08-14 17:01:12 -0300

    testando uma classe service

--- src/test/java/br/com/alura/adopet/api/service/AdocaoServiceTest.java
+++ src/test/java/br/com/alura/adopet/api/service/AdocaoServiceTest.java
@@ -1,18 +1,28 @@
 package br.com.alura.adopet.api.service;
 
+import br.com.alura.adopet.api.dto.SolicitacaoAdocaoDto;
+import br.com.alura.adopet.api.model.Abrigo;
+import br.com.alura.adopet.api.model.Pet;
+import br.com.alura.adopet.api.model.Tutor;
 import br.com.alura.adopet.api.repository.AdocaoRepository;
 import br.com.alura.adopet.api.repository.PetRepository;
 import br.com.alura.adopet.api.repository.TutorRepository;
 import br.com.alura.adopet.api.validacoes.ValidacaoSolicitacaoAdocao;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.then;
 
+@ExtendWith(MockitoExtension.class)
 class AdocaoServiceTest {
     @InjectMocks
     private AdocaoService service;
@@ -32,13 +42,27 @@ class AdocaoServiceTest {
     @Mock
     private List<ValidacaoSolicitacaoAdocao> validacoes;
 
+    @Mock
+    private Pet pet;
+
+    @Mock
+    private Tutor tutor;
+
+    @Mock
+    private Abrigo abrigo;
+
+    @Mock
+    private SolicitacaoAdocaoDto dto;
+
     @Test
     void deveriaSalvarAdocaoAoSolicitar() {
         //ARRANGE
 
         //ACT
+        service.solicitar(dto);
 
         //ASSERT
+        then(petRepository).should().save(any());
     }
 
 }
