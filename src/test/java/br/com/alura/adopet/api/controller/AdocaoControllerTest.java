364d4554c0b5b84dc0ed2c5c7a5b2db20cc64577
Lucca Pedroza <luccaesu2003@hotmail.com>
2026-08-19 16:29:19 -0300

    testando uma classe controller

---
+++ src/test/java/br/com/alura/adopet/api/controller/AdocaoControllerTest.java
@@ -0,0 +1,22 @@
+package br.com.alura.adopet.api.controller;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AdocaoControllerTest {
+
+
+    @Test
+    void deveriaDevolverCodigo400ParaSolicitacaoDeAdocaoComErros() {
+        //ARRANGE
+        String json = "{}";
+
+        //ACT
+
+        //ASSERT
+        Assertions.assertEquals(400, ?);
+    }
+
+}
